b8b29d77117633e51c5780ceea2eb3fd18fccfae
Victor David Cano <119651209+vicDcano@users.noreply.github.com>
2026-09-01 19:44:28 -0700

    Update Vite config for base path and input file

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,17 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/', 
+  base: './', 
   plugins: [react()],
   build: {
     rollupOptions: {
       input: {
-        main: resolve(import.meta.dirname, 'home.html'), 
+        main: resolve(import.meta.dirname, 'index.html'), 
         project: resolve(import.meta.dirname, 'project.html'), 
       },
     },
   },
-  // This native configuration automatically rewrites clean paths while you test locally!
+  
   server: {
     configureServer(server) {
       server.middlewares.use((req, res, next) => {
